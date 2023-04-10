1ab539bd9d20895367542d5188a6f771546e904f
pandalee <pandaleecn@gmail.com>
2023-04-10 20:41:31 +0800

    111

--- go-ipa-installer.rb
+++ go-ipa-installer.rb
@@ -1,8 +1,8 @@
 class GoIpaInstaller < Formula
   desc "Go IPA Installer service for installing iOS apps over HTTP"
   homepage "https://github.com/pandaleecn/go_ipa_installer" # Replace 'pandaleecn' with your actual GitHub username
-  url "https://github.com/pandaleecn/go_ipa_installer/archive/refs/tags/v1.0.0.tar.gz" # Replace with the correct URL to the tarball of the release
-  sha256 "a1c09c92165a7625b5c10d546c2a6b7951ce1a2ebbb819936bfbc5d0b4141dfa"
+  url "https://github.com/pandaleecn/go_ipa_installer/archive/refs/tags/v1.0.1.tar.gz" # Replace with the correct URL to the tarball of the release
+  sha256 "bf391aaea092f8f877cfdd61af8457f963c603a8184b62b6f0d9d51653f4aee3"
   license "MIT"
 
   depends_on "go" => :build
